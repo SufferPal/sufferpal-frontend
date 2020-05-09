a39a40da539047e43fd7f781987198f9af1127f1
Jacob Mikrut <jmikrut08@gmail.com>
2020-05-09 17:39:17 -0500

    setup router

---
+++ src/pages/CalendarPage/CalendarPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import DefaultTemplate from '../../templates/DefaultTemplate/DefaultTemplate';
+
+const CalendarPage = () => {
+  return <DefaultTemplate>CalendarPage</DefaultTemplate>;
+};
+
+export default CalendarPage;
--- src/templates/DefaultTemplate/DefaultTemplate.js
+++ src/templates/DefaultTemplate/DefaultTemplate.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import Header from '../../components/Header/Header';
+import { Container } from 'reactstrap';
+import PropTypes from 'prop-types';
 
-const DefaultTemplate = () => {
-  return <p>Hello world</p>;
+const DefaultTemplate = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Container>{children}</Container>
+      </main>
+    </>
+  );
 };
+
+DefaultTemplate.propTypes = { children: PropTypes.element.isRequired };
 
 export default DefaultTemplate;
