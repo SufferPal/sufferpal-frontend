3e003ca58ba205639f154382c18f84503b312428
jdahl13 <jdahl07@gmail.com>
2020-05-25 13:47:13 -0500

    styled card data

--- src/components/ActivityCard/ActivityCard.js
+++ src/components/ActivityCard/ActivityCard.js
@@ -16,27 +16,47 @@ const ActivityCard = (props) => {
   }, []);
 
   return (
-    <div className="ActivityCard w-100 py-2 px-4">
+    <div className="ActivityCard w-100 py-2 px-4 mb-3">
       <Row>
-        <Col sm="3">
-          <div className="profile-picture-container"></div>
-          <h3 className="user-name">
-            {firstName}
-            {lastName}
+        <Col sm="3" className="d-flex flex-column justify-content-start align-items-center">
+          <div className="profile-picture-container mb-2 "></div>
+          <h3 className="user-name mb-2">
+            {firstName} {lastName}
           </h3>
-          {activity?.description && <p className="activity-description">{activity?.description}</p>}
+          {activity?.description && <p className="activity-description text-center">{activity?.description}</p>}
         </Col>
         <Col sm="9">
           <Row>
-            <Col sm="12" className="data-column">
-              <h3>{totalDistance.toFixed(2)}</h3>
-              <h3>{calculatePace(totalMovingTime, totalDistance)}</h3>
-              <h3>{createCustomTimeString(totalMovingTime)}</h3>
-              <h3>{avgHeartRate}</h3>
+            <Col sm="12" className="data-column d-flex justify-content-center align-items-center px-3">
+              <div className="data-header-container d-flex justify-content-center align-items-center flex-column p-1 mr-1">
+                <h3 className="data-header">
+                  {totalDistance.toFixed(2)}
+                  <span className="units">mi</span>
+                </h3>
+                <h4 className="data-label">Distance</h4>
+              </div>
+              <div className="data-header-container d-flex justify-content-center align-items-center flex-column p-1 mr-1">
+                <h3 className="data-header">
+                  {calculatePace(totalMovingTime, totalDistance)}
+                  <span className="units">/mi</span>
+                </h3>
+                <h4 className="data-label">Pace</h4>
+              </div>
+              <div className="data-header-container d-flex justify-content-center align-items-center flex-column p-1 mr-1">
+                <h3 className="data-header">{createCustomTimeString(totalMovingTime)}</h3>
+                <h4 className="data-label">Run Time</h4>
+              </div>
+              <div className="data-header-container d-flex justify-content-center align-items-center flex-column p-1 mr-1">
+                <h3>
+                  {avgHeartRate}
+                  <span className="units">bpm</span>
+                </h3>
+                <h4 className="data-label">Avg Heartrate</h4>
+              </div>
             </Col>
           </Row>
           <Row>
-            <Col sm="12">
+            <Col sm="12" className="d-flex justify-content-center align-items-center">
               <MapContainer rawMeasurements={rawMeasurements} />
             </Col>
           </Row>
