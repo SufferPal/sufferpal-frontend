fa4612db1c872a55424edba78ae594207eb226d6
Jacob Mikrut <jmikrut08@gmail.com>
2020-05-31 14:01:19 -0500

    graph data created

---
+++ src/components/SpeedGraph/SpeedGraph.js
@@ -0,0 +1,10 @@
+import React from 'react';
+//import { useLocation } from 'react-router-dom';
+
+const SpeedGraph = (activity) => {
+  console.log('speedData record', activity);
+
+  return <div></div>;
+};
+
+export default SpeedGraph;
--- src/pages/ActivityDetailPage/ActivityDetailPage.js
+++ src/pages/ActivityDetailPage/ActivityDetailPage.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Row, Col } from 'reactstrap';
 import DefaultTemplate from '../../templates/DefaultTemplate/DefaultTemplate';
+import SpeedGraph from '../../components/SpeedGraph/SpeedGraph';
+import Storage from '@aws-amplify/storage';
 
 const ActivityDetailPage = () => {
   const location = useLocation();
   const activityState = location.state?.activity;
 
-  console.log(activityState);
+  const [rawMeasurements, setRawMeasurements] = useState([]);
+  const S3_Key = activityState.rawMeasurementsS3FileKey;
+  const speedData = [[]];
+  const HRData = [[]];
+  const cadenceData = [[]];
+
+  useEffect(() => {
+    //console.log('activity', activity);
+    Storage.get(S3_Key, { download: true }).then((result) => {
+      setRawMeasurements(result.Body);
+    });
+  }, []);
+
+  const dataArray = () => {
+    for (let i = 1; i < rawMeasurements.length; i++) {
+      //console.log(rawMeasurements[i].speed);
+      if (rawMeasurements[i].speed !== null) {
+        speedData.push([rawMeasurements[i].speed, rawMeasurements[i].distance]);
+      }
+      if (rawMeasurements[i].speed !== null) {
+        HRData.push([rawMeasurements[i].heart_rate, rawMeasurements[i].distance]);
+      }
+      if (rawMeasurements[i].speed !== null) {
+        cadenceData.push([rawMeasurements[i].cadence, rawMeasurements[i].distance]);
+      }
+    }
+  };
+
+  dataArray();
+  console.log(speedData);
+  console.log(HRData);
+  console.log(cadenceData);
+  //console.log(activityState);
 
   return (
     <DefaultTemplate>
@@ -15,6 +49,7 @@ const ActivityDetailPage = () => {
         <Row>
           <Col md="2"></Col>
           <Col md="10">
+            <SpeedGraph activity={speedData} />
             {/* <GraphContainer acivity={activityState} /> */}
             {/* const GraphContainer = (activity) => {} */}
           </Col>
