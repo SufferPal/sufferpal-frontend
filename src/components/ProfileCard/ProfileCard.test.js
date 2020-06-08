1d2c78702d3f62fed2040a290909eea45bdf0c11
Daniel Pencak <danielpencak@gmail.com>
2020-06-08 17:46:50 -0500

    Update test

--- src/components/ProfileCard/ProfileCard.test.js
+++ src/components/ProfileCard/ProfileCard.test.js
@@ -5,11 +5,12 @@ import { Provider } from 'react-redux';
 import { store } from '../../store/store';
 
 const fetchUser = jest.fn();
+const isModalButtonDisabled = false;
 
 test('renders ProfileCard', () => {
   const { getByText } = render(
     <Provider store={store}>
-      <ProfileCard fetchUser={fetchUser} />
+      <ProfileCard fetchUser={fetchUser} isModalButtonDisabled={isModalButtonDisabled} />
     </Provider>
   );
   const profileCardText = getByText(/user settings/i);
